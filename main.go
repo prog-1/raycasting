20e1f5212f11e545f23f9fdd677447821d53c503
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-03-31 18:54:47 +0300

    Added character

--- main.go
+++ main.go
@@ -9,34 +9,64 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Pair[T, U any] struct {
+	X T
+	Y U
+}
+
 const (
 	sw = 640
 	sh = 480
-	mw = 10 // map width
-	mh = 10 // map height
+	mw = 24 // map width
+	mh = 24 // map height
 )
 
 type game struct {
 	screenBuffer *ebiten.Image
 	maze         [mh][mw]int
+	playerPos    Pair[int, int]
 }
 
 func NewGame() *game {
-	return &game{
+	g := &game{
 		ebiten.NewImage(sw, sh),
 		[mh][mw]int{
-			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 1, 1, 1, 0, 0, 0, 0, 1},
-			{1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
-			{1, 0, 1, 1, 0, 0, 1, 1, 1, 1},
-			{1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
-			{1, 1, 1, 1, 0, 0, 1, 0, 1, 1},
-			{1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
-			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		},
+		Pair[int, int]{},
 	}
+	g.getInitialPlayerPos()
+	return g
+}
+
+func (g *game) getInitialPlayerPos() {
+	var x, y int
+	for x, y = 0, 0; g.maze[y][x] != 0; x, y = x+1, y+1 {
+	}
+	g.playerPos = Pair[int, int]{x, y}
 }
 
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return sw, sh }
@@ -44,14 +74,15 @@ func (g *game) Update() error {
 	return nil
 }
 func (g *game) Draw(screen *ebiten.Image) {
-	cubeSize := v.Vec{sw / mw, sh / mh, 0}
+	cubeSize := v.Vec{sw / float64(mw), sh / float64(mh), 0}
 	for y := range g.maze {
 		for x := range g.maze[y] {
 			if g.maze[y][x] != 0 {
-				ebitenutil.DrawRect(screen, cubeSize.X*float64(x), float64(y)*cubeSize.Y, cubeSize.X, cubeSize.Y, color.White)
+				ebitenutil.DrawRect(screen, cubeSize.X*float64(x), float64(y)*cubeSize.Y, cubeSize.X, cubeSize.Y, color.RGBA{255, 192, 203, 255})
 			}
 		}
 	}
+	ebitenutil.DrawRect(screen, cubeSize.X*float64(g.playerPos.X), cubeSize.Y*float64(g.playerPos.Y), cubeSize.X, cubeSize.Y, color.RGBA{255, 255, 0, 255})
 }
 
 func main() {
