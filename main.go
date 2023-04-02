416253033db870696d9ede8367a84cb1f3662c01
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-04-02 09:59:18 +0300

    Added speed to player

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"time"
 
 	v "github.com/34thSchool/vectors"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -10,10 +11,9 @@ import (
 )
 
 const (
-	sw = 640
-	sh = 480
-	mw = 24 // map width
-	mh = 24 // map height
+	sw, sh   = 640, 480
+	mw, mh   = 24, 24                             // map width/height
+	sqw, sqh = float64(sw) / mw, float64(sh) / mh // square width/height
 )
 
 type Pair[T, U any] struct {
@@ -22,101 +22,120 @@ type Pair[T, U any] struct {
 }
 
 type Player struct {
-	Index Pair[int, int]
+	Pos   v.Vec
 	Dir   v.Vec
+	speed float64
 }
 
 type game struct {
-	screenBuffer *ebiten.Image
-	maze         *[mh][mw]int
-	p            Player
+	screenBuffer  *ebiten.Image
+	maze          *[mh][mw]int
+	p             Player
+	prevFrameTime int64
 }
 
 func NewGame() *game {
+	// Values on the map:
+	// 0 - empty space
+	// 1 - player(yellow)
+	// 2 - white wall
+	// 3 - pink wall
+	// 4 - green wall
 	maze := [mh][mw]int{
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+		{2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 	}
 	return &game{
 		ebiten.NewImage(sw, sh),
 		&maze,
 		InitPlayer(maze),
+		0,
 	}
 }
 
 func InitPlayer(maze [mh][mw]int) Player {
 	var x, y int
-	for x, y = 0, 0; maze[y][x] != 0; x, y = x+1, y+1 {
+	for y = range maze {
+		for x = range maze[y] {
+			if maze[y][x] == 0 {
+				break
+			}
+		}
+		if maze[y][x] == 0 {
+			break
+		}
 	}
-	return Player{Pair[int, int]{x, y}, v.Vec{1, 0, 0}}
+
+	return Player{v.Vec{float64(x)*sqw + sqw/2, float64(y)*sqh + sqh/2, 0}, v.Vec{1, 0, 0}, 0.2}
 }
 
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return sw, sh }
 func (g *game) Update() error {
+	dt := float64(time.Now().UnixMilli() - g.prevFrameTime)
+	g.prevFrameTime = time.Now().UnixMilli()
+
 	// Movement:
+	in := Pair[int, int]{int(g.p.Pos.X / sqw), int(g.p.Pos.Y / sqh)} // pos = in * sqw => in = pos/sqw
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		if g.maze[g.p.Index.Y-1][g.p.Index.X] == 0 {
-			g.p.Index.Y--
+		if g.maze[in.Y-1][in.X] == 0 {
+			g.p.Pos.Y -= g.p.speed * dt
 		}
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		if g.maze[g.p.Index.Y+1][g.p.Index.X] == 0 {
-			g.p.Index.Y++
-		}
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		if g.maze[g.p.Index.Y][g.p.Index.X+1] == 0 {
-			g.p.Index.X++
+		if g.maze[in.Y+1][in.X] == 0 {
+			g.p.Pos.Y += g.p.speed * dt
 		}
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		if g.maze[g.p.Index.Y][g.p.Index.X-1] == 0 {
-			g.p.Index.X--
+		if g.maze[in.Y][in.X-1] == 0 {
+			g.p.Pos.X -= g.p.speed * dt
+		}
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		if g.maze[in.Y][in.X+1] == 0 {
+			g.p.Pos.X += g.p.speed * dt
 		}
 	}
 
 	// Rotation
 	if ebiten.IsKeyPressed(ebiten.KeyQ) {
-
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyE) {
+		g.p.Dir.RotateZ(0.01 * dt)
 	}
 	return nil
 }
 func (g *game) Draw(screen *ebiten.Image) {
-	cubeSize := v.Vec{sw / float64(mw), sh / float64(mh), 0}
 	for y := range g.maze {
 		for x := range g.maze[y] {
 			if g.maze[y][x] != 0 {
-				ebitenutil.DrawRect(screen, cubeSize.X*float64(x), float64(y)*cubeSize.Y, cubeSize.X, cubeSize.Y, color.RGBA{255, 192, 203, 255})
+				ebitenutil.DrawRect(screen, sqw*float64(x), float64(y)*sqh, sqw, sqh, color.RGBA{255, 192, 203, 255})
 			}
 		}
 	}
-	playerCenterPos := v.Vec{cubeSize.X*float64(g.p.Index.X) + cubeSize.X/2, cubeSize.Y*float64(g.p.Index.Y) + cubeSize.Y/2, 0}
-	ebitenutil.DrawRect(screen, cubeSize.X*float64(g.p.Index.X), cubeSize.Y*float64(g.p.Index.Y), cubeSize.X, cubeSize.Y, color.RGBA{255, 255, 0, 255})
-	ebitenutil.DrawLine(screen, playerCenterPos.X, playerCenterPos.Y, playerCenterPos.X+g.p.Dir.X*10, playerCenterPos.Y-g.p.Dir.Y*10, color.RGBA{124, 252, 0, 255})
+	in := Pair[int, int]{int(g.p.Pos.X / sqw), int(g.p.Pos.Y / sqh)} // pos = in * sqw => in = pos/sqw
+	ebitenutil.DrawRect(screen, sqw*float64(in.X), sqh*float64(in.Y), sqw, sqh, color.RGBA{255, 255, 0, 255} /*yellow*/)
+	ebitenutil.DrawLine(screen, g.p.Pos.X, g.p.Pos.Y, g.p.Pos.X+g.p.Dir.X*10, g.p.Pos.Y-g.p.Dir.Y*10, color.RGBA{124, 252, 0, 255})
 }
 
 func main() {
