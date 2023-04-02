a815fa3cf0dc89199df7b7a812245287be8772b3
Richard Shashalevich <richardsasalevic@gmail.com>
2023-04-02 22:09:15 +0300

    Added player

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -10,7 +11,7 @@ import (
 
 const (
 	width  = 640.0
-	height = 480.0
+	height = 640.0
 )
 
 var (
@@ -42,7 +43,7 @@ var (
 )
 
 type Point struct {
-	x, y int
+	x, y float64
 }
 
 type Game struct {
@@ -53,16 +54,30 @@ type Game struct {
 func (g *Game) Update() error {
 	return nil
 }
+func Rotate(a Point, angle float64) Point {
+	a.x, a.y = a.x*math.Cos(angle)-a.y*math.Sin(angle), a.x*math.Sin(angle)+a.y*math.Cos(angle)
+	return a
+}
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	// ebitenutil.DebugPrint(screen, "Hello, World!")
-
+func DrawMap(screen *ebiten.Image) {
 	for i := range maze {
 		for j, WallType := range maze[i] {
 			if WallType != 0 {
 				ebitenutil.DrawRect(screen, float64(j*oneBlockWidthLength), float64(i*oneBlockHeightLength), float64(oneBlockWidthLength), float64(oneBlockHeightLength), color.RGBA{255, 0, 0, 255})
 			}
 		}
+	}
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// ebitenutil.DebugPrint(screen, "Hello, World!")
+	DrawMap(screen)
+
+	ebitenutil.DrawCircle(screen, g.pos.x, g.pos.y, 3, color.RGBA{255, 255, 0, 255})
+
+	for i := -30.0; i < 31; i += 0.5 {
+		tmp := Rotate(g.dir, i*math.Pi/180)
+		ebitenutil.DrawLine(screen, g.pos.x, g.pos.y, g.pos.x+tmp.x, g.pos.y+tmp.y, color.RGBA{255, 255, 0, 255})
 	}
 }
 
@@ -73,7 +88,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(width, height)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&Game{Point{320, 350}, Point{0, -1000}}); err != nil {
 		log.Fatal(err)
 	}
 }
