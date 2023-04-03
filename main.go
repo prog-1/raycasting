87c55dfe82b23e8a39726c4db54d31cb72c4a6e4
Andrej Ceremnih <andrejceremnih@gmail.com>
2023-04-03 08:51:22 +0300

    not done

---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+const (
+	screenWidth  = 700
+	screenHeight = 700
+)
+
+// Point is a struct for representing 2D vectors.
+type Point struct {
+	x, y float64
+}
+
+type Player struct {
+	pos Point
+	dir Point
+}
+
+type Maze struct {
+}
+
+var Map = [][]int{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+}
+
+// NewPlayer initializes and returns a new Player instance.
+func NewPlayer(x, y float64) *Player {
+	return &Player{
+		pos: Point{x, y},
+	}
+}
+
+func (p *Player) Update() {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && p.pos.y > 15+8 {
+		p.pos.y -= 1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && p.pos.y < 15*23-8 {
+		p.pos.y += 1
+	}
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+
+}
+
+// Game is a game instance.
+type Game struct {
+	width, height int
+	Player        *Player
+	Maze          *Maze
+	mapIsOpen     bool
+}
+
+// NewGame returns a new Game instance.
+func NewGame(width, height int) *Game {
+	return &Game{
+		width:  width,
+		height: height,
+		Player: NewPlayer(15*12, 15*23-8),
+	}
+}
+
+func (g *Game) Layout(outWidth, outHeight int) (w, h int) {
+	return g.width, g.height
+}
+
+// Update updates a game state.
+func (g *Game) Update() error {
+	g.Player.Update()
+	return nil
+}
+
+func DrawMap(img *ebiten.Image, pos Point) {
+	for i := range Map {
+		for j := range Map[i] {
+			c := color.RGBA{}
+			if Map[i][j] == 1 {
+				c = color.RGBA{0xff, 0xff, 0xff, 0xff}
+			} else if Map[i][j] == 2 {
+				c = color.RGBA{0x0E, 0x30, 0x7F, 0xff}
+			} else if Map[i][j] == 3 {
+				c = color.RGBA{0x0A, 0x51, 0x0C, 0xff}
+			} else if Map[i][j] == 4 {
+				c = color.RGBA{0x78, 0x0A, 0x0A, 0xff}
+			}
+			ebitenutil.DrawRect(img, float64(15*i), float64(15*j), 15, 15, c)
+		}
+	}
+	ebitenutil.DrawCircle(img, pos.x, pos.y, 8, color.White)
+}
+
+// Draw renders a game screen.
+func (g *Game) Draw(screen *ebiten.Image) {
+	if inpututil.IsKeyJustPressed(ebiten.KeyM) && !g.mapIsOpen {
+		g.mapIsOpen = true
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyM) {
+		g.mapIsOpen = false
+	}
+	if g.mapIsOpen {
+		DrawMap(screen, g.Player.pos)
+	}
+	g.Player.Draw(screen)
+}
+
+func main() {
+	//rand.Seed(time.Now().UnixNano())
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	g := NewGame(screenWidth, screenHeight)
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
