a69cc0d283b2c2050d30bc36dcca93313e36d860
Ričards Šašalevičs <rsasalevics@edu.riga.lv>
2023-04-19 14:47:07 +0300

    Improved map

--- main.go
+++ main.go
@@ -110,15 +110,67 @@ func DrawMap(screen *ebiten.Image) {
 	}
 }
 
+func abs(a float64) float64 {
+	if a < 0 {
+		return a * -1
+	}
+	return a
+}
+
+func raycast(screen *ebiten.Image, pos Point, dirx, diry float64) int {
+	mapX, mapY := int(pos.x), int(pos.y)
+
+	deltaDistX := abs(1 / dirx)
+	deltaDistY := abs(1 / diry)
+	var sideDistX, sideDistY float64
+	var stepX, stepY int
+	if dirx < 0 {
+		stepX = -1
+		sideDistX = (pos.x - float64(mapX)) * deltaDistX
+	} else {
+		stepX = 1
+		sideDistX = (float64(mapX) + 1.0 - pos.x) * deltaDistX
+	}
+	if diry < 0 {
+		stepY = -1
+		sideDistY = (pos.y - float64(mapY)) * deltaDistY
+	} else {
+		stepY = 1
+		sideDistY = (float64(mapY) + 1.0 - pos.y) * deltaDistY
+	}
+	var side int
+	for {
+		//jump to next map square, either in x-direction, or in y-direction
+		if sideDistX < sideDistY {
+			sideDistX += deltaDistX
+			mapX += stepX
+			side = 0
+		} else {
+			sideDistY += deltaDistY
+			mapY += stepY
+			side = 1
+		}
+		//Check if ray has hit a wall
+		// fmt.Println(mapX, mapY)
+		if maze[mapY/int(oneBlockHeightLength)][mapX/int(oneBlockWidthLength)] > 0 {
+			ebitenutil.DrawLine(screen, pos.x, pos.y, float64(mapX), float64(mapY), color.RGBA{255, 255, 0, 255})
+
+			return side
+		}
+	}
+}
+
 func (g *Game) Draw(screen *ebiten.Image) {
 	// ebitenutil.DebugPrint(screen, "Hello, World!")
 	DrawMap(screen)
 
 	ebitenutil.DrawCircle(screen, g.pos.x, g.pos.y, 3, color.RGBA{255, 255, 0, 255})
 
-	for i := -30.0; i < 31; i += 0.5 {
+	for i := -30.0; i < 31; i += 0.01 {
 		tmp := Rotate(g.dir, i*math.Pi/180)
-		ebitenutil.DrawLine(screen, g.pos.x, g.pos.y, g.pos.x+tmp.x*1000, g.pos.y+tmp.y*1000, color.RGBA{255, 255, 0, 255})
+		// ebitenutil.DrawLine(screen, g.pos.x, g.pos.y, g.pos.x+tmp.x*1000, g.pos.y+tmp.y*1000, color.RGBA{255, 255, 0, 255})
+		raycast(screen, g.pos, tmp.x, tmp.y)
+
 	}
 
 	// ebitenutil.DrawRect(screen, float64(a*oneBlockWidthLength), float64(b*oneBlockHeightLength), float64(oneBlockWidthLength), float64(oneBlockHeightLength), color.RGBA{0, 0, 255, 255})
