c0f85f29172d39c3addc359a36327e1637845071
Sp0ozy <imolchanov210@gmail.com>
2023-04-22 16:15:33 +0300

    Revert "in progress"
    This reverts commit d122754d31ed63b50db9415268a5d5186aad67f3.

--- main.go
+++ main.go
@@ -49,35 +49,23 @@ func (g *game) Update() error {
 		g.rotation -= math.Pi / 100
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
-		if g.pg[((int(g.p.y)-38)/50)+1][int(g.p.x)/50] == 0 {
-			tmp := &point{5, 0}
-			rotate(tmp, g.rotation)
-			g.p.y += tmp.y
-			g.p.x += tmp.x
+		if g.pg[((int(g.p.y)+500-38)/50)+1][int(g.p.x+500)/50] == 0 {
+			g.p.y += 5
 		}
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
-		if g.pg[(int(g.p.y))/50][int(g.p.x+38)/50-1] == 0 {
-			tmp := &point{0, -5}
-			rotate(tmp, g.rotation)
-			g.p.y += tmp.y
-			g.p.x += tmp.x
+		if g.pg[(int(g.p.y)+500)/50][int(g.p.x+500+38)/50-1] == 0 {
+			g.p.x -= 5
 		}
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
-		if g.pg[(int(g.p.y))/50][int(g.p.x-38)/50+1] == 0 {
-			tmp := &point{0, 5}
-			rotate(tmp, g.rotation)
-			g.p.y += tmp.y
-			g.p.x += tmp.x
+		if g.pg[(int(g.p.y)+500)/50][int(g.p.x+500-38)/50+1] == 0 {
+			g.p.x += 5
 		}
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
-		if g.pg[(int(g.p.y+38))/50-1][int(g.p.x)/50] == 0 {
-			tmp := &point{-5, 0}
-			rotate(tmp, g.rotation)
-			g.p.y += tmp.y
-			g.p.x += tmp.x
+		if g.pg[(int(g.p.y+38)+500)/50-1][int(g.p.x+500)/50] == 0 {
+			g.p.y -= 5
 		}
 	}
 
@@ -85,46 +73,15 @@ func (g *game) Update() error {
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
-	var op ebiten.DrawImageOptions
-	// w, h := g.m.Size()
+	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(-g.p.x, -g.p.y)
-	op.GeoM.Rotate(g.rotation)
-	op.GeoM.Translate(screenWidth/2, screenHeight/2)
-	screen.DrawImage(g.m, &op)
+	screen.DrawImage(g.m, op)
 	ebitenutil.DrawCircle(screen, screenWidth/2, screenHeight/2, 10, color.RGBA{0xff, 0xff, 0x00, 0xff})
-	for i := 0.0; i < 400; i++ {
-		var side, step point
-		cameraX := i/200.0 - 1
-		ray := point{g.dir.x + g.plane.x*cameraX, g.dir.y + g.plane.y*cameraX}
-		p := point{g.p.x, g.p.y}
-		delta := point{math.Abs(1 / ray.x), math.Abs(1 / ray.y)}
-		if ray.x < 0 {
-			step.x = -1
-			side.x = (g.p.x - p.x) * delta.x
-		} else {
-			step.x = 1
-			side.x = (p.x + 1.0 - g.p.x) * delta.x
-		}
-		if ray.y < 0 {
-			step.y = -1
-			side.y = (g.p.y - p.y) * delta.y
-		} else {
-			step.y = 1
-			side.y = (p.y + 1.0 - g.p.y) * delta.y
-		}
-		for g.pg[int(p.y)/50][int(p.x)/50] == 0 {
-			if side.x < side.y {
-				side.x += delta.x
-				p.x += step.x
-			} else {
-				side.y += delta.y
-				p.y += step.y
-			}
-		}
-		p1 := point{p.x - screenWidth, p.y - screenHeight}
-		rotate(&p1, -g.rotation)
-		p1 = point{p1.x + screenWidth, p1.y + screenHeight}
-		ebitenutil.DrawLine(screen, screenWidth/2, screenHeight/2, screenWidth/2+p1.x, screenHeight/2+p1.y, color.RGBA{0xff, 0xff, 0x00, 0xff})
+	pnt := g.lp
+	diff := g.rp.x - g.lp.x
+	for pnt.x <= g.rp.x {
+		ebitenutil.DrawLine(screen, screenWidth/2, screenHeight/2, pnt.x+screenWidth/2, pnt.y+screenHeight/2, color.RGBA{0xff, 0xff, 0x00, 0xff})
+		pnt.x += diff * 0.01
 	}
 
 }
@@ -182,10 +139,10 @@ func NewGame() *game {
 	}
 
 	return &game{
-		m:     DrawBackground(ebiten.NewImage(screenWidth, screenHeight), pg),
-		p:     point{100, 100},
-		pg:    pg,
-		dir:   point{1, 0},
-		plane: point{0.5, 0.5},
+		m:  DrawBackground(ebiten.NewImage(screenWidth, screenHeight), pg),
+		p:  point{0, 0},
+		pg: pg,
+		lp: point{-200, 200},
+		rp: point{200, 200},
 	}
 }
