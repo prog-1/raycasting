5594c23dccc2af1a59a74308938bf859f9c09adf
Sp0ozy <imolchanov210@gmail.com>
2023-05-07 15:37:24 +0300

    no textures

--- main.go
+++ main.go
@@ -7,13 +7,14 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 const (
 	winTitle     = "Cube"
-	screenWidth  = 960
-	screenHeight = 720
-	cellSize     = 8
+	screenWidth  = 625
+	screenHeight = 625
+	cellSize     = 5
 	dpi          = 100
 )
 
@@ -26,6 +27,7 @@ type (
 		p, dir, plane point
 		pg            [][]int
 		fisheye       bool
+		showMap       bool
 	}
 )
 
@@ -42,126 +44,141 @@ func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, 
 func (g *game) Update() error {
 
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		rotate(&g.dir, -math.Pi/180)
+		rotate(&g.dir, -math.Pi/100)
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		rotate(&g.dir, math.Pi/180)
+		rotate(&g.dir, math.Pi/100)
 
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && g.pg[int(g.p.y/8-g.dir.y/8)][int(g.p.x/8-g.dir.x/8)] == 0 {
-		g.p.x -= g.dir.x / 8
-		g.p.y -= g.dir.y / 8
+	if inpututil.IsKeyJustPressed(ebiten.KeyM) {
+		g.showMap = !g.showMap
+
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && g.pg[int(g.p.y/8-g.dir.x/8)][int(g.p.x/8+g.dir.y/8)] == 0 {
-		g.p.x += g.dir.y / 8
-		g.p.y -= g.dir.x / 8
+	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
+		g.fisheye = !g.fisheye
+
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && g.pg[int(g.p.y/8+g.dir.x/8)][int(g.p.x/8-g.dir.y/8)] == 0 {
-		g.p.x -= g.dir.y / 8
-		g.p.y += g.dir.x / 8
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && g.pg[int(g.p.y/8+g.dir.y/8)][int(g.p.x/8+g.dir.x/8)] == 0 {
-		g.p.x += g.dir.x / 8
-		g.p.y += g.dir.y / 8
+	for i := 0; i < 6; i++ {
+		if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && g.pg[int(g.p.y/25-g.dir.y/8)][int(g.p.x/25-g.dir.x/8)] == 0 {
+			g.p.x -= g.dir.x / 8
+			g.p.y -= g.dir.y / 8
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && g.pg[int(g.p.y/25-g.dir.x/8)][int(g.p.x/25+g.dir.y/8)] == 0 {
+			g.p.x += g.dir.y / 8
+			g.p.y -= g.dir.x / 8
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && g.pg[int(g.p.y/25+g.dir.x/8)][int(g.p.x/25-g.dir.y/8)] == 0 {
+			g.p.x -= g.dir.y / 8
+			g.p.y += g.dir.x / 8
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && g.pg[int(g.p.y/25+g.dir.y/8)][int(g.p.x/25+g.dir.x/8)] == 0 {
+			g.p.x += g.dir.x / 8
+			g.p.y += g.dir.y / 8
+		}
 	}
 
 	return nil
 }
-func drawWalls(screen *ebiten.Image, dist, wall, m float64, col color.RGBA) {
+func drawWalls(screen *ebiten.Image, dist, wall float64, col color.RGBA) {
 
-	ebitenutil.DrawLine(screen, wall, screenWidth/2, wall, (screenWidth/2)+(screenWidth/2)*4/dist, col)
-	ebitenutil.DrawLine(screen, wall, screenWidth/2, wall, (screenWidth/2)-(screenWidth/2)*4/dist, col)
-
-	for tmp := wall; tmp > wall-m; tmp -= m / 20 {
-		ebitenutil.DrawLine(screen, tmp, screenWidth/2, tmp, (screenWidth/2)+(screenWidth/2)*4/dist, col)
-		ebitenutil.DrawLine(screen, tmp, screenWidth/2, tmp, (screenWidth/2)-(screenWidth/2)*4/dist, col)
-	}
+	ebitenutil.DrawLine(screen, wall, screenHeight/2, wall, (screenHeight/2)+(5000)/dist, col)
+	ebitenutil.DrawLine(screen, wall, screenHeight/2, wall, (screenHeight/2)-(5000)/dist, col)
 
 }
 func (g *game) Draw(screen *ebiten.Image) {
-	screen.DrawImage(g.m, nil)
-	ebitenutil.DrawCircle(screen, g.p.x, g.p.y, 3, color.RGBA{0xff, 0xff, 0x00, 0xff})
-	var wall float64
-	m := float64(screenWidth) / 60
-	for i := 0.0; i <= 60; i++ {
+
+	gap := 60.0 / (float64(screenWidth) - 1)
+	for i, wall := -30.0, 0.0; i <= 30; i, wall = i+gap, wall+1 {
 		var side, step point
+		var dist float64
 		var ray = g.dir
-		rotate(&ray, math.Pi/180*(i-30))
-		p := point{math.Floor(g.p.x), math.Floor(g.p.y)}
+		rotate(&ray, math.Pi/180*(i))
+		maP := point{math.Floor(g.p.x), math.Floor(g.p.y)}
 		delta := point{math.Abs(1 / ray.x), math.Abs(1 / ray.y)}
 		if ray.x < 0 {
 			step.x = -1
-			side.x = (g.p.x - float64(int(p.x))) * delta.x
+			side.x = (g.p.x - float64(int(maP.x))) * delta.x
 		} else {
 			step.x = 1
-			side.x = (float64(int(p.x)) + 1.0 - g.p.x) * delta.x
+			side.x = (float64(int(maP.x)) + 1.0 - g.p.x) * delta.x
 		}
 		if ray.y < 0 {
 			step.y = -1
-			side.y = (g.p.y - float64(int(p.y))) * delta.y
+			side.y = (g.p.y - float64(int(maP.y))) * delta.y
 		} else {
 			step.y = 1
-			side.y = (float64(int(p.y)) + 1.0 - g.p.y) * delta.y
+			side.y = (float64(int(maP.y)) + 1.0 - g.p.y) * delta.y
 		}
 		shadow := true
-		for g.pg[int(p.y)/cellSize][int(p.x)/cellSize] == 0 {
+		for g.pg[int(maP.y)/25][int(maP.x)/25] == 0 {
 			if side.x < side.y {
 				side.x += delta.x
-				p.x += step.x
+				maP.x += step.x
 				shadow = true
 			} else {
 				side.y += delta.y
-				p.y += step.y
+				maP.y += step.y
 				shadow = false
 			}
-			screen.Set(int(p.x), int(p.y), color.RGBA{255, 255, 0, 255})
 
 		}
-		dist := math.Sqrt(math.Pow(p.x/8*25-g.p.x/8*25, 2) + math.Pow(p.y/8*25-g.p.y/8*25, 2))
-		if side.x < side.y {
-			side.x += delta.x
-			p.x += step.x
+		if g.fisheye {
+			if shadow {
+				dist = side.x - delta.x
+			} else {
+				dist = side.y - delta.y
+			}
 		} else {
-			side.y += delta.y
-			p.y += step.y
+			//I invented something
+			if shadow {
+				dist = (side.x - delta.x) * math.Cos(math.Pi/180*(i))
+			} else {
+				dist = (side.y - delta.y) * math.Cos(math.Pi/180*(i))
+			}
 		}
+		// dist /= 20
 		if shadow {
-			switch g.pg[int(p.y/8)][int(p.x/8)] {
+			switch g.pg[int(maP.y/25)][int(maP.x/25)] {
 			case 1:
-				drawWalls(screen, dist, wall, m, color.RGBA{0xff, 0xff, 0xff, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{0xff, 0xff, 0xff, 0xff})
 
 			case 2:
-				drawWalls(screen, dist, wall, m, color.RGBA{0xff, 0, 0, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{0xff, 0, 0, 0xff})
 			case 3:
-				drawWalls(screen, dist, wall, m, color.RGBA{0, 0xff, 0, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{0, 0xff, 0, 0xff})
 
 			case 4:
-				drawWalls(screen, dist, wall, m, color.RGBA{0, 0, 0xff, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{0, 0, 0xff, 0xff})
 
 			case 5:
-				drawWalls(screen, dist, wall, m, color.RGBA{227, 61, 148, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{227, 61, 148, 0xff})
 			}
 
 		} else {
-			switch g.pg[int(p.y/8)][int(p.x/8)] {
+			switch g.pg[int(maP.y/25)][int(maP.x/25)] {
 			case 1:
-				drawWalls(screen, dist, wall, m, color.RGBA{100, 100, 100, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{100, 100, 100, 0xff})
 
 			case 2:
-				drawWalls(screen, dist, wall, m, color.RGBA{100, 0, 0, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{100, 0, 0, 0xff})
 			case 3:
-				drawWalls(screen, dist, wall, m, color.RGBA{0, 100, 0, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{0, 100, 0, 0xff})
 
 			case 4:
-				drawWalls(screen, dist, wall, m, color.RGBA{0, 0, 100, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{0, 0, 100, 0xff})
 
 			case 5:
-				drawWalls(screen, dist, wall, m, color.RGBA{100, 30, 60, 0xff})
+				drawWalls(screen, dist, wall, color.RGBA{100, 30, 60, 0xff})
 			}
 		}
+		if g.showMap {
+			ebitenutil.DrawLine(screen, g.p.x/cellSize, g.p.y/cellSize, maP.x/cellSize, maP.y/cellSize, color.RGBA{255, 255, 0, 255})
+		}
 
-		wall += m
-
+	}
+	if g.showMap {
+		screen.DrawImage(g.m, nil)
+		ebitenutil.DrawCircle(screen, g.p.x/cellSize, g.p.y/cellSize, 3, color.RGBA{0xff, 0xff, 0x00, 0xff})
 	}
 
 }
@@ -204,7 +221,8 @@ func DrawBackground(m *ebiten.Image, pg [][]int) *ebiten.Image {
 	return m
 }
 func NewGame() *game {
-	pg := [][]int{{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	pg := [][]int{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -231,11 +249,10 @@ func NewGame() *game {
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}}
 
 	return &game{
-		m:       DrawBackground(ebiten.NewImage(200, 200), pg),
-		p:       point{100, 100},
+		m:       DrawBackground(ebiten.NewImage(screenWidth/cellSize, screenHeight/cellSize), pg),
+		p:       point{screenWidth / 2, screenHeight / 2},
 		pg:      pg,
 		dir:     point{1, -1},
-		plane:   point{0.5, 0.5},
 		fisheye: true,
 	}
 }
